38dec418ee31e13e13f8fe295a9804439956a311
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-30 12:15:48 +0600

    added a post api to store data in the database

--- index.js
+++ index.js
@@ -15,9 +15,20 @@ const client = new MongoClient(uri, {
     }
 });
 async function run() {
+
+    const bookCollection = client.db('bookswap').collection('allbooks'); 
+
+
+    app.post("/books",  async(req, res) => {
+        const book = req.body; 
+        const result = await bookCollection.insertOne(book)
+        res.send(result)
+    })
+
+    app.get("/", (req, res) => {
+        res.send("hello world")
+    })
     try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
@@ -27,9 +38,6 @@ run().catch(console.dir);
 
 
 
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
 
 
 app.listen(port, () => {
