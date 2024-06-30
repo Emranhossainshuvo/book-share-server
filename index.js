2199cdf164d065730c4f1ffd80b3475c7f592856
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-30 12:37:09 +0600

    cors installed and configured to ignore cross platform problems

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
